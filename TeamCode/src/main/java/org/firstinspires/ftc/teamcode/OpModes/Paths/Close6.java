6d7bc2be6b3dffc505bdb9f25edc4fd553df871f
Tixlol2 <144851993+Tixlol2@users.noreply.github.com>
2026-01-19 16:36:36 -0600

    new auto construction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Paths/Close6.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Paths/Close6.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode.OpModes.Paths;
 
 import org.firstinspires.ftc.teamcode.Util.Poses;
+import org.firstinspires.ftc.teamcode.Util.Subsystems.Robot;
 
 public class Close6 extends MainPaths {
 
     public Close6() {
-        super("Close 6", 4, ShootingLocation.SHORT, Poses.blueGoalTopStartFacing);
+        super("Close 6", 4, ShootingLocation.SHORT, Robot.color == Robot.teamColor.BLUE ? Poses.blueGoalTopStartFacing : Poses.redGoalTopStartFacing);
         init();
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Paths/Close9.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Paths/Close9.java
@@ -1,11 +1,12 @@
 package org.firstinspires.ftc.teamcode.OpModes.Paths;
 
 import org.firstinspires.ftc.teamcode.Util.Poses;
+import org.firstinspires.ftc.teamcode.Util.Subsystems.Robot;
 
 public class Close9 extends MainPaths {
 
     public Close9() {
-        super("Close 9", 4, ShootingLocation.SHORT, Poses.blueGoalTopStartFacing);
+        super("Close 9", 6, ShootingLocation.SHORT, Robot.color == Robot.teamColor.BLUE ? Poses.blueGoalTopStartFacing : Poses.redGoalTopStartFacing);
         init();
     }
 
